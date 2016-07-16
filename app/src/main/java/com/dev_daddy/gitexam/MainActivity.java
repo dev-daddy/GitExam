a91ae5fd713b2d7088c4903ffa1a845770543387
kittoboy <kittoboy@naver.com>
2016-07-16 21:14:45 +0900

    Hello World!!!!

---
+++ app/src/main/java/com/dev_daddy/gitexam/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dev_daddy.gitexam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "Hello World");
+    }
+}
